3e9c99cd4dc5f74135534b0e3ff266f3938d1ad0
Imran Hasan <imranhasan871@gmail.com>
2023-02-19 00:36:23 +0600

    feat Added counter raw component

---
+++ html_template/script.js
@@ -0,0 +1,33 @@
+// select dom elements
+const counterEl = document.getElementById('counter');
+const incrementEl = document.getElementById('increment');
+const decrementEl = document.getElementById('decrement');
+
+const counterEl2 = document.getElementById('counter2');
+const incrementEl2 = document.getElementById('increment2');
+const decrementEl2 = document.getElementById('decrement2');
+
+// initial state
+let count = 0;
+let count2 = 0;
+
+// add addEventListener
+incrementEl.addEventListener('click', () => {
+	count++;
+	counterEl.innerText = count;
+});
+
+decrementEl.addEventListener('click', () => {
+	count--;
+	counterEl.innerText = count;
+});
+
+incrementEl2.addEventListener('click', () => {
+	count2++;
+	counterEl2.innerText = count2;
+});
+
+decrementEl2.addEventListener('click', () => {
+	count2--;
+	counterEl2.innerText = count2;
+});
